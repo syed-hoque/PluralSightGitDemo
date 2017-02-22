383723c70869a6ddd8859e8b66a1fe7b3fddfafe
Syed Hoque <syed.hoque@itineris.co.uk>
2017-02-22 14:12:25 +0000

    Moved to V2 branch

---
+++ VS2015_Demo/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS2015_Demo
+{
+    class MyClass
+    {
+    }
+}
